3b416cc17d64371a08e4bc22377266cc3ae41e80
noumssiprisca6-star <noumssiprisca6@gmail.com>
2026-09-18 01:14:44 +0200

    modification ligne 5 du repertoire 2 SecondRep MonEssaie .

--- exo1_modification/Fichier3.cpp
+++ exo1_modification/Fichier3.cpp
@@ -3,6 +3,6 @@ int Soustraction(int a, int b) {
     return a - b;
 }
 int main(){
-    int result = Soustraction(11,12);
+    int result = Soustraction(12,12);
     return 0; 
 }
--- exo1_modification/main.cpp
+++ exo1_modification/main.cpp
@@ -2,6 +2,6 @@
 int main (){
     /*affiche*/
     std::cout<<"testons les commandes d'annulation de git  "<<std::endl;
-    std::cout <<"le test des erreurs sur github et de leur annulation "<<std::endl;
+    std::cout <<"verifion les modifications differentes sur deux repertoires avec le fonctionnement de git "<<std::endl;
     return 0;
 }
