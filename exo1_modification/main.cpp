c0b2c46f5fbd8e5c520e085735d6368acb34276f
noumssiprisca6-star <noumssiprisca6@gmail.com>
2026-09-18 13:48:49 +0200

    ajout de la fonction merge

--- exo1_modification/main.cpp
+++ exo1_modification/main.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 int main (){
     /*affiche*/
-    std::cout<<"la fonction merge  "<<std::endl;
+    std::cout<<"la fonction merge .  "<<std::endl;
     std::cout <<"le test des erreurs sur github et de leur annulation "<<std::endl;
     return 0;
 }
