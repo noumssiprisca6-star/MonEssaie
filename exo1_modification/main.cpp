afcc158864db683d917d6d159498cf863993d071
noumssiprisca6-star <noumssiprisca6@gmail.com>
2026-09-18 13:52:15 +0200

    ajout de la fonction rebase

--- exo1_modification/main.cpp
+++ exo1_modification/main.cpp
@@ -2,6 +2,6 @@
 int main (){
     /*affiche*/
     std::cout<<"la fonction merge .  "<<std::endl;
-    std::cout <<"le test des erreurs sur github et de leur annulation "<<std::endl;
+    std::cout <<"le test des erreurs sur github et de leur annulation . "<<std::endl;
     return 0;
 }
