042ed95617cb5c195bcfe3dc1e80002c82bf5ab4
noumssiprisca6-star <noumssiprisca6@gmail.com>
2026-09-19 13:49:50 +0200

    modification du message sur feature

--- mes_demos/test.cpp
+++ mes_demos/test.cpp
@@ -1,6 +1,5 @@
 # include <iostream>
 int main (){
-    std::cout <<"fichier pour realiser la premiere demostration des exercice sur mon depot afin de desssiner un tableau de graphe de mes commits ."<<std::endl;
+    std::cout <<" testons"<<std::endl;
     return 0 ;
-
 }
