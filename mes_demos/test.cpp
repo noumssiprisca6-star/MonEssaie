a2d84645d94e6eda318a26d08d9368fda86d9dcb
noumssiprisca6-star <noumssiprisca6@gmail.com>
2026-09-19 12:05:57 +0200

    initialisation du projet

---
+++ mes_demos/test.cpp
@@ -0,0 +1,6 @@
+# include <iostream>
+int main (){
+    std::cout <<"fichier pour realiser la premiere demostration des exercice sur mon depot afin de desssiner un tableau de graphe de mes commits "<<std::endl;
+    return 0 ;
+
+}
