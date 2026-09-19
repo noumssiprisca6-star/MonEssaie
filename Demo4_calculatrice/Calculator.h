174c190cb9daa951c3fbdcb4f32b405dea90b02c
noumssiprisca6-star <noumssiprisca6@gmail.com>
2026-09-19 22:06:21 +0200

    la declaration des methodes necessaire pour l'implementation des fonctions de calcul simple

---
+++ Demo4_calculatrice/Calculator.h
@@ -0,0 +1,15 @@
+#ifndef CALCULATOR.H
+#define CALCULATOR.H
+
+class Calculator
+{
+public:
+//constructeur 
+    Calculator();
+    //Destructeur 
+    ~Calculator();
+    //Methodes
+    int Addition (int x, int y);
+    double Division(double a ,double b);
+};
+#endif
